8fe849f2a16a01c19c352e69cc1da7357b74b3f6
Ignas Dauksys <ignasdauksys@protonmail.com>
2025-08-14 20:26:23 -0500

    edited home page and navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,15 +1,17 @@
-import { Link } from "react-router-dom"
-export default function NavBar(){
-    return(
-        <>
-            <div className="flex flex-row justify-evenly">
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+    return (
+        <div className="flex flex-row items-center ml-auto mb-10 bg-gray-500 pb-2">
+            <h2 className="text-4xl">Password Manager</h2>
+            <div className="flex flex-row space-x-6 ml-8 text-2xl">
                 <Link to="/">
-                    <h3>Home</h3>
+                    <h3 className={"hover:bg-gray-600"}>Home</h3>
                 </Link>
                 <Link to="/login">
-                    <h3>Login</h3>
+                    <h3 className={"hover:bg-gray-600"}>Login</h3>
                 </Link>
             </div>
-        </>
-    )
-};
+        </div>
+    );
+}
