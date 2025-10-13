2cba1f4e86117c713f5c926495a133560c080dfb
Ignas Dauksys <ignasdauksys@protonmail.com>
2025-10-13 12:57:10 -0500

    add .gitignore and untrack .env

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,18 +1,25 @@
 from flask import Flask
 from .auth.routes import auth_bp
 from .extensions import db, jwt, cors
+from dotenv import load_dotenv
+import os
 
 
-def create_app(config_class="app.config.DevConfig"):
+def create_app():
+    load_dotenv()
     app = Flask(__name__)
-    app.config.from_object(config_class)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SECRET_KEY"] = "supersecretkey"
 
-    jwt.init_app(app)
+    if os.getenv("FLASK_ENV") == "production":
+        from app.config import ProdConfig
+        app.config.from_object(ProdConfig)
+    else:
+        from app.config import DevConfig
+        app.config.from_object(DevConfig)
+
     db.init_app(app)
     cors.init_app(app, resources={r"/*": {"origins": "http://localhost:5173"}}, support_credentials=True)
+    jwt.init_app(app)
+
     # Register blueprints
     app.register_blueprint(auth_bp, url_prefix="/auth")
     #app.register_blueprint(vault_bp, url_prefix="/vault")
